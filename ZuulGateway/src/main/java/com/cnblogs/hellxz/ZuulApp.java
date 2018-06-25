0cd9cc7731fc66529caed066f098d4000f83df89
Hellxz <hellxz001@foxmail.com>
2018-06-25 18:50:39 +0800

    zuul项目初始化

---
+++ ZuulGateway/src/main/java/com/cnblogs/hellxz/ZuulApp.java
@@ -0,0 +1,14 @@
+package com.cnblogs.hellxz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringCloudApplication
+public class ZuulApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApp.class, args);
+    }
+}
