32c1ee1258e0ea80568011db66fe7e53a151e2d4
Hellxz <hellxz001@foxmail.com>
2018-07-09 11:23:51 +0800

    加注释

--- ConfigServer/src/main/java/ConfigServerApp.java
+++ ConfigServer/src/main/java/ConfigServerApp.java
@@ -3,8 +3,13 @@ import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * 这里没有使用SpringBootApplication或SpringCloudApplication注解，会报错
+ * 原因也很简单，我们的java源码目录下没有目录，我们手动加一个也就正常了，
+ * 为了写点体会和这里没必要用到包和类，所以使用这种方式
+ */
 @EnableConfigServer
-@SpringBootConfiguration //这里没有使用SpringBootApplication或SpringCloudApplication注解，会报错
+@SpringBootConfiguration
 @EnableAutoConfiguration
 public class ConfigServerApp {
 
