2350e297103b3a937e8344d998eb31b265c63fce
Hellxz <hellxz001@foxmail.com>
2018-07-09 18:13:37 +0800

    提交Config Client部分代码

--- ConfigClient/src/main/java/com/cnblogs/hellxz/controller/TestController.java
+++ ConfigClient/src/main/java/com/cnblogs/hellxz/controller/TestController.java
@@ -3,17 +3,22 @@ package com.cnblogs.hellxz.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RefreshScope
+@RefreshScope //开启更新功能
+@RequestMapping("api")
 public class TestController {
 
     @Value("${from}")
     private String fromValue;
 
-    @GetMapping("/fromValue")
+    /**
+     * 返回配置文件中的值
+     */
+    @GetMapping("/from")
     @ResponseBody
     public String returnFormValue(){
         return fromValue;
