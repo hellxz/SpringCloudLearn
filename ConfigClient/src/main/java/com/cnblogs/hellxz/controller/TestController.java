9e10ab3b128b294607f563326e3829883d1cd571
Hellxz <hellxz001@foxmail.com>
2018-07-10 11:55:43 +0800

    加注释

--- ConfigClient/src/main/java/com/cnblogs/hellxz/controller/TestController.java
+++ ConfigClient/src/main/java/com/cnblogs/hellxz/controller/TestController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 当有请求/fresh节点的时候，会重新请求一次ConfigServer去拉取最新的配置文件
+ * 请求/fresh需要有几点要求：1.加actuator的依赖 2.SpringCloud1.5以上需要设置 management.security.enabled=false
+ * 这个Controller的作用是查看from这个key的值
+ */
 @RestController
 @RefreshScope //开启更新功能
 @RequestMapping("api")
