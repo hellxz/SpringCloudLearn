79b688b53c7c950b006d98fe670d0878e75d2bfa
Hellxz <hellxz001@foxmail.com>
2018-07-26 08:27:29 +0800

    Stream项目初始化

---
+++ LoggingConsumer/src/main/java/com/cnblogs/hellxz/LoggingConsumer/LoggingConsumerApplication.java
@@ -0,0 +1,35 @@
+package com.cnblogs.hellxz.LoggingConsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+
+@SpringBootApplication
+@EnableBinding(Sink.class)
+public class LoggingConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggingConsumerApplication.class, args);
+	}
+
+	@StreamListener(Sink.INPUT)
+	public void handle(Person person) {
+		System.out.println("Received: " + person);
+	}
+
+	public static class Person {
+		private String name;
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String toString() {
+			return this.name;
+		}
+	}
+
+}
