583adb81df08ba9df3923d9d4ef3f6ee479b5a55
Hellxz <hellxz001@foxmail.com>
2018-07-28 14:51:02 +0800

    stream 入门项目加测试

--- LoggingConsumer/src/main/java/com/cnblogs/hellxz/LoggingConsumer/LoggingConsumerApplication.java
+++ LoggingConsumer/src/main/java/com/cnblogs/hellxz/LoggingConsumer/LoggingConsumerApplication.java
@@ -6,6 +6,9 @@ import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
 
+/**
+ * Spring Stream 入门项目
+ */
 @SpringBootApplication
 @EnableBinding(Sink.class)
 public class LoggingConsumerApplication {
--- LoggingConsumer/src/test/java/com/cnblogs/hellxz/LoggingConsumer/LoggingConsumerApplicationTests.java
+++ LoggingConsumer/src/test/java/com/cnblogs/hellxz/LoggingConsumer/LoggingConsumerApplicationTests.java
@@ -2,15 +2,23 @@ package com.cnblogs.hellxz.LoggingConsumer;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+import static junit.framework.TestCase.assertNotNull;
+
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class LoggingConsumerApplicationTests {
 
+	@Autowired
+	private Sink sink;
+
 	@Test
 	public void contextLoads() {
+		assertNotNull(this.sink.input());
 	}
-
 }
